f412aacb928bca6f7e8dbd42c9b5a30cf85db4b7
Sam Guerrero <samguerrero@Sams-MacBook-Pro.local>
2019-02-25 19:52:09 -0800

    get pics

--- apps/voice_app/views.py
+++ apps/voice_app/views.py
@@ -19,13 +19,11 @@ owm = OWM(API_key)
 def talkToMe(phrase):
         tts = gTTS(text=phrase, lang="en")
         tts.save("audio.mp3")
-        os.system("audio.mp3")
+        os.system("mpg123 audio.mp3")
         Phrase.objects.create(content = phrase)
 
 def postImage(phrase):
     Phrase.objects.create(content = phrase)
-
-
 
 def index(request):
     itemlist = ItemList.objects.all().order_by("-id")
@@ -60,11 +58,9 @@ def myCommand(request):
     request.session["command"] = command
     return request.session["command"]
 
-
 def voice(request):
     talkToMe("whats your command")
     command = myCommand(request)
-
 
     # if "open Reddit python" in command:
     #         chrome_path = "open -a /Applications/Google\ Chrome.app %s"
@@ -75,7 +71,6 @@ def voice(request):
         joke = requests.get('https://geek-jokes.sameerkumar.website/api')
         print(joke.text)
         talkToMe(joke.text)
-                
 
     if 'current weather' in command:
         talkToMe("What city")
@@ -93,10 +88,36 @@ def voice(request):
         # location = weather.lookup_by_location("seattle")
         # condition = location.condition
         # talkToMe('The Current weather in %s is %s The tempeture is %.1f degree' % (city, condition.text(), (int(condition.temp())-32)/1.8))
-        talkToMe("weather is " + str(status) + " with a temerature of " + str(temp["temp"]) + " degrees")
+        talkToMe("current weather in "+ city + " is " + str(status) + " with a temerature of " + str(temp["temp"]) + " degrees")
+
+    if 'fox' in command.lower():
+        print("Sam, this is the command")
+        print(command)
+        talkToMe(command)
+        flickrApiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3fe4879a5cbb64c72bd1c73499e6c9dd&per_page=12&tags=" + command + "&tag_mode=any&format=json&nojsoncallback=1"
+        print(flickrApiUrl)
+        flickr_res = requests.get(flickrApiUrl)
+        print("OH SHIT")
+        print(flickr_res.json()['photos']['photo'])
+        all_pics = []
+        for i in flickr_res.json()['photos']['photo']:
+            all_pics.append(i['id'])
+        print(all_pics)
+        formated_pics = []
+        for m in all_pics:
+            url_complete = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3fe4879a5cbb64c72bd1c73499e6c9dd&photo_id=" + m + "&format=json&nojsoncallback=1"
+            img_res = requests.get(url_complete)
+            images = img_res.json()['sizes']['size']
+            print("SET")
+            for j in images:
+                if j['label'] == 'Medium':
+                    print(j['source'])
+                    formated_pics.append('<img src="{}" alt="things" height="200" width="200">'.format(j['source']))
+        request.session['main_content'] = formated_pics
+
     elif 'how are you' in command:
             talkToMe("I am good, thanks!")
-    
+
     elif 'joke' in command:
         res = requests.get(
                 'https://icanhazdadjoke.com/',
@@ -106,6 +127,7 @@ def voice(request):
             talkToMe(str(res.json()['joke']))
         else:
             talkToMe('oops!I ran out of jokes')
+
     elif 'open website' in command:
         reg_ex = re.search('open website (.+)', command)
         if reg_ex:
@@ -115,12 +137,8 @@ def voice(request):
             print('Done!')
         else:
             pass
-    elif 'cat' in command:
-        postImage("http://pngimg.com/uploads/cat/cat_PNG50509.png")
-        talkToMe("current weather in "+ city + " is " + str(status) + " with a temerature of " + str(temp["temp"]) + " degrees")
+    # elif 'cat' in command:
+    #     postImage("http://pngimg.com/uploads/cat/cat_PNG50509.png")
 
-    
     return redirect("/")
 
-    
-    
