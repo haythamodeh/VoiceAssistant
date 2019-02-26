c342b981a6060e8ea0454b30fdfe678d86f0aada
Vlad Dziun <v.dziun@gmail.com>
2019-02-25 16:01:42 -0800

    vlad commit

--- apps/voice_app/models.py
+++ apps/voice_app/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class ItemList(models.Model):
     item = models.TextField()
+
+class Phrase(models.Model):
+    content = models.TextField()
--- apps/voice_app/views.py
+++ apps/voice_app/views.py
@@ -6,7 +6,8 @@ import webbrowser
 from weather import Weather
 import re
 from pyowm import OWM
-from .models import ItemList
+from .models import ItemList, Phrase
+import requests
 
 API_key = '1b22d51d2689d3610710583b11cb5fdd'
 owm = OWM(API_key)
@@ -16,13 +17,20 @@ owm = OWM(API_key)
 def talkToMe(phrase):
         tts = gTTS(text=phrase, lang="en")
         tts.save("audio.mp3")
-        os.system("mpg123 audio.mp3")
+        os.system("audio.mp3")
+        Phrase.objects.create(content = phrase)
+
+def postImage(phrase):
+    Phrase.objects.create(content = phrase)
+
 
 
 def index(request):
     itemlist = ItemList.objects.all().order_by("-id")
+    phrases = Phrase.objects.last()
     content = {
-        "all_items": itemlist
+        "all_items": itemlist,
+        "last_phrase": phrases
     }
     return render(request, "voice_app/index.html", content)
 
@@ -38,8 +46,8 @@ def voice(request):
     try:
         print("You said: " + r.recognize_google(audio))
         print("Thank You!")
-        talkToMe("You said " + r.recognize_google(audio))
-        talkToMe("Thank You")
+        # talkToMe("You said " + r.recognize_google(audio))
+        # talkToMe("Thank You")
         # print("You said in arabic: " + r.recognize_google(audio, language = "ar-AR"))
 
     except:
@@ -48,6 +56,7 @@ def voice(request):
     phrase = r.recognize_google(audio)
     command = r.recognize_google(audio)
     ItemList.objects.create(item = phrase)
+
 
     # if "open Reddit python" in command:
     #         chrome_path = "open -a /Applications/Google\ Chrome.app %s"
@@ -76,6 +85,29 @@ def voice(request):
         # condition = location.condition
         # talkToMe('The Current weather in %s is %s The tempeture is %.1f degree' % (city, condition.text(), (int(condition.temp())-32)/1.8))
         talkToMe("weather is " + str(status) + " with a temerature of " + str(temp["temp"]) + " degrees")
+    elif 'how are you' in command:
+            talkToMe("I am good, thanks!")
+    
+    elif 'joke' in command:
+        res = requests.get(
+                'https://icanhazdadjoke.com/',
+                headers={"Accept":"application/json"}
+                )
+        if res.status_code == requests.codes.ok:
+            talkToMe(str(res.json()['joke']))
+        else:
+            talkToMe('oops!I ran out of jokes')
+    elif 'open website' in command:
+        reg_ex = re.search('open website (.+)', command)
+        if reg_ex:
+            domain = reg_ex.group(1)
+            url = 'https://www.' + domain
+            webbrowser.open(url)
+            print('Done!')
+        else:
+            pass
+    elif 'cat' in command:
+        postImage("http://pngimg.com/uploads/cat/cat_PNG50509.png")
 
     # talkToMe(phrase)
     # words.append(phrase)
